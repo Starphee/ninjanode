cc098d4fac134d1990d86e7ab67e432f196a3eb2
Starphee <33707294+Starphee@users.noreply.github.com>
2025-08-19 09:47:35 -0700

    Ship Renderer Changes
    - renderer changes for new assets
    - projectile file changes to allow for new projectiles.

--- public/modules/pixi/PixiProjectile.mjs
+++ public/modules/pixi/PixiProjectile.mjs
@@ -6,12 +6,17 @@
 import { projectileTypes } from 'data';
 
 const {
-    Sprite,
-    Container,
-    Texture,
-    filters: { GlowFilter },
+<<<<<<< HEAD
+  Sprite,
+  Container,
+  Texture,
+=======
+  Container,
+  Graphics,
+  FillGradient,
+>>>>>>> parent of b6c6d12 (New Assets Dump)
+  filters: { GlowFilter },
 } = window.PIXI;
-
 
 const degToRad = (degrees) => degrees * (Math.PI / 180);
 
@@ -27,88 +32,134 @@ export class PixiProjectile {
   ticker;
 
   constructor({ app, style, type, parent, pos = {} }) {
-      this.config = projectileTypes[type];
-      this.app = app;
-      this.type = type;
-      this.style = style;
-      this.started = Date.now();
-      this.container = new Container();
-      parent.addChild(this.container);
+    this.config = projectileTypes[type];
+    this.app = app;
+    this.type = type;
+    this.style = style;
+    this.started = Date.now();
+    this.container = new Container();
+    parent.addChild(this.container);
 
-      // Create the sprite using pre-loaded texture
-      const textureAlias = `laser-${this.style}`;
-      const texture = Texture.from(textureAlias);
-      const sprite = new Sprite(texture);
+<<<<<<< HEAD
+    // Create the sprite using pre-loaded texture
+    const textureAlias = `laser-${this.style}`;
+    const texture = Texture.from(textureAlias);
+    const sprite = new Sprite(texture);
 
-      // Set anchor to center for rotation.
-      sprite.anchor.set(0.5);
+    // Set anchor to center for rotation.
+    sprite.anchor.set(0.5);
 
-        // Scale the sprite by the type.
-      const { width, height } = this.config.size;
-        sprite.width = width;
-        sprite.height = height;
+    // Scale the sprite by the type.
+    const { width, height } = this.config.size;
+    sprite.width = width;
+    sprite.height = height;
 
-      this.container.addChild(sprite);
+    this.container.addChild(sprite);
 
-      this.container.filters = [
-          new GlowFilter({ distance: 10, outerStrength: 5 }),
-        ];
+    this.container.filters = [
+      new GlowFilter({ distance: 10, outerStrength: 5 }),
+    ];
 
-      this.setPos(pos);
+    this.setPos(pos);
 
-      // Velocity is locked at init.
-      this.velocity = {
-        x: (this.config.speed / 1000) * Math.cos(degToRad(this.pos.d - 90)),
-        y: (this.config.speed / 1000) * Math.sin(degToRad(this.pos.d - 90)),
-      };
+    // Velocity is locked at init.
+    this.velocity = {
+      x: (this.config.speed / 1000) * Math.cos(degToRad(this.pos.d - 90)),
+      y: (this.config.speed / 1000) * Math.sin(degToRad(this.pos.d - 90)),
+    };
 
-      this.initTicker();
-    }
+=======
+    const laser = new Graphics();
+    this.container.addChild(laser);
+    const width = 5;
+    const length = 50;
+    const color = 0xff0000;
 
-    initTicker() {
-      this.ticker = () => {
-        this.tickerCallback();
-      };
+    // Bottom of laser
+    laser.moveTo(0, 50);
 
-      this.app.ticker.add(this.ticker);
-    }
+    // Top Left
+    laser.lineTo(-width / 2, -length / 2);
 
-    tickerCallback() {
-      const deltaMs = this.app.ticker.deltaMS;
-        // Glide between vector velocity length updates.
-      if (this.container && !this.container.destroyed) {
-        this.container.updateTransform({
-          x: this.container.x + this.velocity.x * deltaMs,
-          y: this.container.y + this.velocity.y * deltaMs,
-        });
-      }
-    }
+    // Rounded arc to right.
+    laser.arcTo(0, -length / 2 - 5, width / 2 + 10, -length / 2 + 20, width);
 
-    destroy() {
-      // TODO: Anything else to clean up?
-      this.app.ticker.remove(this.ticker);
-      this.container.destroy();
-    }
+    // Back to bottom.
+    laser.lineTo(0, length / 2);
 
-    setPos({ x = 0, y = 0, d = 0 } = {}) {
-      if (!this.container) return;
-      this.pos.x = x;
-      this.pos.y = y;
-      this.pos.d = d;
+    // Create a fill gradient
+    const gradientFill = new FillGradient(-width, 0, width, 0);
 
-      this.container.updateTransform({ x, y });
-      this.container.rotation = degToRad(d);
-    }
+    // Add the color stops to the fill gradient
+    gradientFill.addColorStop(0, 0xff0000);
+    gradientFill.addColorStop(0.5, 0xffffff);
+    gradientFill.addColorStop(1, 0xff0000);
 
-    setActive(state) {
-      this.active = !!state;
-    }
+    laser.fill(gradientFill);
 
-    activate() {
-      this.setActive(true);
-    }
+    this.container.filters = [
+      new GlowFilter({ distance: 20, outerStrength: 5, color }),
+    ];
+    this.setPos(pos);
 
-    deactivate() {
-      this.setActive(false);
+    // Velocity is locked at init.
+    this.velocity = {
+      x: (this.config.speed / 1000) * Math.cos(degToRad(this.pos.d - 90)),
+      y: (this.config.speed / 1000) * Math.sin(degToRad(this.pos.d - 90)),
+    };
+
+>>>>>>> parent of b6c6d12 (New Assets Dump)
+    this.initTicker();
+  }
+
+  initTicker() {
+    this.ticker = () => {
+      this.tickerCallback();
+    };
+
+    this.app.ticker.add(this.ticker);
+  }
+
+  tickerCallback() {
+    const deltaMs = this.app.ticker.deltaMS;
+    // Glide between vector velocity length updates.
+    if (this.container && !this.container.destroyed) {
+      this.container.updateTransform({
+        x: this.container.x + this.velocity.x * deltaMs,
+        y: this.container.y + this.velocity.y * deltaMs,
+      });
+<<<<<<< HEAD
+      //  this.container.rotation += 0.01 * deltaMs;
+=======
+>>>>>>> parent of b6c6d12 (New Assets Dump)
     }
   }
+
+  destroy() {
+    // TODO: Anything else to clean up?
+    this.app.ticker.remove(this.ticker);
+    this.container.destroy();
+  }
+
+  setPos({ x = 0, y = 0, d = 0 } = {}) {
+    if (!this.container) return;
+    this.pos.x = x;
+    this.pos.y = y;
+    this.pos.d = d;
+
+    this.container.updateTransform({ x, y });
+    this.container.rotation = degToRad(d);
+  }
+
+  setActive(state) {
+    this.active = !!state;
+  }
+
+  activate() {
+    this.setActive(true);
+  }
+
+  deactivate() {
+    this.setActive(false);
+  }
+}
